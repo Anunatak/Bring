3480cae0f76c3459bcd6103276f83e7038864714
Tor Morten Jensen <tormorten@anunatak.no>
2015-02-19 14:54:53 +0100

    add file modules

---
+++ src/Anunatak/Bring/Bring.php
@@ -0,0 +1,123 @@
+<?php namespace Anunatak\Bring;
+
+use GuzzleHttp\Client;
+use App;
+
+class Bring {
+
+	/**
+	 * The endpoint to the API for most calls
+	 * 
+	 * @var string Endpoint URL
+	 * @access protected
+	 */
+	protected static $endpoint = 'https://api.bring.com/';
+
+	/**
+	 * The endpoint to the API for tracking calls
+	 * 
+	 * @var string Endpoint URL
+	 * @access protected
+	 */
+	protected static $tracking = 'http://sporing.bring.no/';
+
+	/**
+	 * Retrieve information about a postcode
+	 * 
+	 * @param int $postalCode A valid postal code
+	 * @param string $country The country code
+	 * 
+	 * @return array An array of data
+	 * @access public
+	 */
+	public static function postalCode( $postalCode, $country = 'no' ) {
+
+		// create query args
+		$query 		= array(
+			'clientUrl'		=> App::make('url')->to('/'),
+			'country'		=> (string)$country,
+			'pnr'			=> (int)$postalCode
+		);
+
+		// the url base
+		$url 		= self::$endpoint . 'shippingguide/api/postalCode.json?' . http_build_query($query);
+
+		return self::request($url);
+
+	}
+
+	/**
+	 * Retrieves list of the alternative locations for package pickup near a postal code
+	 * 
+	 * @param int $postalCode A valid postal code
+	 * @param string $country The country code
+	 * 
+	 * @return array An array of pickup points
+	 * @access public
+	 */
+	public static function pickupPoints( $postalCode, $country = 'no' ) {
+
+		// the country needs to be uppercase
+		$country 	= strtoupper($country);
+
+		// create an url
+		$url 		= self::$endpoint . 'pickuppoint/api/pickuppoint/'. $country . '/postalCode/'. $postalCode .'.json';
+
+		return self::request($url);
+
+	}
+
+	/**
+	 * Retrieves the tracking history of a tracking number
+	 * 
+	 * Only works for packages in Norway
+	 * 
+	 * @param string $trackingNumber A valid tracking number
+	 * 
+	 * @return array An array with tracking history
+	 * @access public
+	 */
+	public static function trackPackage( $trackingNumber ) {
+
+		// create an url
+		$url 		= self::$tracking . 'sporing.json?q='. $trackingNumber;
+
+		return self::request($url, false);
+
+	}
+
+	/**
+	 * Perform a request using the GuzzleHttp Client
+	 * 
+	 * @param string $url The URL
+	 * 
+	 * @return array An array of data
+	 * @access private
+	 */
+	private static function request( $url, $ssl = true ) {
+		
+		// create an instance of a guzzle client
+		$client = new Client();
+
+		$config = [];
+
+		if( $ssl ) {
+			$config = ['config' => [
+		        'curl' => [
+		            CURLOPT_SSLVERSION => 3,
+		            CURLOPT_PORT => 443,
+		            CURLOPT_SSL_CIPHER_LIST => 'SSLv3'
+		        ]
+		    ]];
+		}
+
+		// try getting a response and set the correct curl options
+		$response = $client->get( $url, $config);
+
+		return $response->json();
+
+	}
+
+}
+
+?>
---
+++ src/Anunatak/Bring/BringServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Anunatak\Bring;
+
+use Illuminate\Support\ServiceProvider;
+
+class BringServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return ['Bring'];
+	}
+
+}
